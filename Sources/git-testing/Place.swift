89e70807cb9aeb966b72985e63db5393af7199c2
Elijah Cobb <elijah@elijahcobb.com>
2019-10-18 23:41:18 -0400

    Added user array and print methods.

--- Sources/git-testing/Place.swift
+++ Sources/git-testing/Place.swift
@@ -11,4 +11,19 @@ struct Place : IDable {
 	var id: String
 	var name: String
 	var zipCode: Int
+	var users: [User]
+	
+	/**
+	A helper function to add a user to the place's user array.
+	
+	- Parameter user: A user instance.
+	*/
+	mutating func add(user: User) { users.append(user) }
+	
+	/**
+	Prints all user's names and does nothing else.
+	*/
+	func printEveryone() {
+		for u in users { print("Hey, \(u.name)! You are \(u.age).")}
+	}
 }
