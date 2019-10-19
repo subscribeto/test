7d3d1bee17acd47d3babe5bf94be955ce7d66cff
Elijah Cobb <elijah@elijahcobb.com>
2019-10-18 23:42:06 -0400

    Added dummy implementation.

--- Sources/git-testing/main.swift
+++ Sources/git-testing/main.swift
@@ -1,1 +1,12 @@
+/*
+This is a dummy file and will be called when you run the project.
+*/
 
+let u1 = User(id: "A", name: "Elijah", age: 20, gender: .male)
+let u2 = User(id: "B", name: "Noah", age: 20, gender: .male)
+let u3 = User(id: "C", name: "Trevor", age: 19, gender: .unknown)
+
+var p = Place(id: "A", name: "Michigan Tech", zipCode: 49931, users: [u1, u2])
+p.add(user: u3)
+
+p.printEveryone()
