c7e4a2a6b3069ba9ab4e1ecff3b980c681cb2170
Elijah Cobb <elijah@elijahcobb.com>
2019-10-19 10:47:30 -0400

    added jacob

--- Sources/git-testing/main.swift
+++ Sources/git-testing/main.swift
@@ -5,6 +5,7 @@ This is a dummy file and will be called when you run the project.
 let u1 = User(id: "A", name: "Elijah", age: 20, gender: .male)
 let u2 = User(id: "B", name: "Noah", age: 20, gender: .male)
 let u3 = User(id: "C", name: "Trevor", age: 19, gender: .unknown)
+let u4 = User(id: "D", name: "Jacob", age: 20, gender: .female)
 
 var p = Place(id: "A", name: "Michigan Tech", zipCode: 49931, users: [u1, u2])
 p.add(user: u3)
